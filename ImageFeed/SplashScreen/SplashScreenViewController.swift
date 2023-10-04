84b6647722a2b46e050213b8e0a5b98bf611f212
Ulia Lukash <ulianalukash@yandex.ru>
2023-10-04 22:03:45 +0300

    Sprint 10

---
+++ ImageFeed/SplashScreen/SplashScreenViewController.swift
@@ -0,0 +1,80 @@
+//
+//  SplashScreenViewController.swift
+//  ImageFeed
+//
+//  Created by Uliana Lukash on 04.10.2023.
+//
+
+import Foundation
+import UIKit
+
+final class SplashScreenViewController: UIViewController {
+
+    let ShowAuthenticationScreenSegueIdentifier = "ShowAuthenticationScreen"
+    private let oauth2Service = OAuth2Service()
+    private let oauth2TokenStorage = OAuth2TokenStorage()
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if let token = OAuth2TokenStorage().token {
+            print(token)
+            switchToTabBarController()
+        } else {
+            performSegue(withIdentifier: ShowAuthenticationScreenSegueIdentifier, sender: nil)
+        }
+    }
+}
+
+extension SplashScreenViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Проверим, что переходим на авторизацию
+        if segue.identifier == ShowAuthenticationScreenSegueIdentifier {
+            
+            // Доберёмся до первого контроллера в навигации. Мы помним, что в программировании отсчёт начинается с 0?
+            guard
+                let navigationController = segue.destination as? UINavigationController,
+                let viewController = navigationController.viewControllers[0] as? AuthViewController
+            else { fatalError("Failed to prepare for \(ShowAuthenticationScreenSegueIdentifier)") }
+            
+            // Установим делегатом контроллера наш SplashViewController
+            viewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+           }
+    }
+    
+    private func switchToTabBarController() {
+        // Получаем экземпляр `Window` приложения
+        guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
+        
+        // Cоздаём экземпляр нужного контроллера из Storyboard с помощью ранее заданного идентификатора.
+        let tabBarController = UIStoryboard(name: "Main", bundle: .main)
+            .instantiateViewController(withIdentifier: "TabBarViewController")
+           
+        // Установим в `rootViewController` полученный контроллер
+        window.rootViewController = tabBarController
+    }
+}
+
+extension SplashScreenViewController: AuthViewControllerDelegate {
+    func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String) {
+        dismiss(animated: true) { [weak self] in
+            guard let self = self else { return }
+            self.fetchOAuthToken(code)
+        }
+    }
+    
+    private func fetchOAuthToken(_ code: String) {
+        oauth2Service.fetchOAuthToken(code) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success:
+                self.switchToTabBarController()
+            case .failure:
+                // TODO [Sprint 11]
+                break
+            }
+        }
+    }
+}
