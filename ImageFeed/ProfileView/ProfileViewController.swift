56cc15eed1bc208f0500fd659574b500179d24b2
Ulia Lukash <ulianalukash@yandex.ru>
2023-10-26 12:50:47 +0300

    feat: started on animations

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -20,7 +20,7 @@ final class ProfileViewController: UIViewController {
     private var profileBioLabel: UILabel?
     private let webVVC = WebViewViewController.shared
     private let profileImageService = ProfileImageService.shared
-
+    var animationLayers = Set<CALayer>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,6 +38,8 @@ final class ProfileViewController: UIViewController {
             let profileImageView = UIImageView(image: profileImage)
             return profileImageView
         }()
+        
+        profilePicGradient(profilePicView: profileImageView)
         // - Text labels
         lazy var nameLabel: UILabel = {
             let nameLabel = UILabel()
@@ -171,6 +173,30 @@ final class ProfileViewController: UIViewController {
             window.makeKeyAndVisible()
         }
     
+    private func profilePicGradient(profilePicView: UIImageView) {
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(origin: .zero, size: CGSize(width: 70, height: 70))
+        gradient.locations = [0, 0.1, 0.3]
+        gradient.colors = [
+            UIColor(red: 0.682, green: 0.686, blue: 0.706, alpha: 1).cgColor,
+            UIColor(red: 0.531, green: 0.533, blue: 0.553, alpha: 1).cgColor,
+            UIColor(red: 0.431, green: 0.433, blue: 0.453, alpha: 1).cgColor
+        ]
+        gradient.startPoint = CGPoint(x: 0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        gradient.cornerRadius = 35
+        gradient.masksToBounds = true
+        animationLayers.insert(gradient)
+        profilePicView.layer.addSublayer(gradient)
+        
+        let gradientChangeAnimation = CABasicAnimation(keyPath: "locations")
+        gradientChangeAnimation.duration = 1.0
+        gradientChangeAnimation.repeatCount = .infinity
+        gradientChangeAnimation.fromValue = [0, 0.1, 0.3]
+        gradientChangeAnimation.toValue = [0, 0.8, 1]
+        gradient.add(gradientChangeAnimation, forKey: "locationsChange")
+
+    }
     
 }
 
